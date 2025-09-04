af2ea71e21df201766d0faf0c2e4348d60149f28
Given <zenobiagiven@gmail.com>
2025-09-04 19:36:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,27 @@
-<script>
-    document.onkeydown = function(e) {
-    if(event.keyCode == 123) {
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'F'.charCodeAt(0)){
-    return false;
-    }
-    if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
-    return false;
-    }
-    }
-    </script>
-    <script type="text/javascript">
-        //Script for disabling right click on mouse
-        var message="Function Disabled!";
-        function clickdsb(){
-        if (event.button==2){
-        alert(message);
-        return false;
-        }
-        }
-        function clickbsb(e){
-        if (document.layers||document.getElementById&&!document.all){
-        if (e.which==2||e.which==3){
-        alert(message);
-        return false;
-        }
-        }
-        }
-        if (document.layers){
-        document.captureEvents(Event.MOUSEDOWN);
-        document.onmousedown=clickbsb;
-        }
-        else if (document.all&&!document.getElementById){
-        document.onmousedown=clickdsb;
-        }
-        
-        document.oncontextmenu=new Function("alert(message);return false")
-</script>
+//Prevent right click
+document.oncontextmenu = () => {
+    alert("Jangan klik kanan woi")
+    return false
+}
 
+document.onkeydown = e => {
+    if(e.key == "F12") {
+        alert ("Ojok diinspect web iki.")
+        return false
+    }
+
+    if(e.ctrlKey && e.key == "u") {
+        alert ("Wes gak aneh aneh.")
+        return false
+    }
+
+    if(e.ctrlKey && e.key == "c") {
+        alert ("Wes gak woiii gak aneh aneh.")
+        return false
+    }
+   
+    if(e.ctrlKey && e.key == "v") {
+        alert ("Wes gak aneh aneh wes diam yah.")
+        return false
+
+    }
